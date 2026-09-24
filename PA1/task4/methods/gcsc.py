cbc0894d7d5ee4cc2791393f3a2e6b2297a04db4
Wasay Jehangiri <wasayjehangiri123@gmail.com>
2026-09-24 20:54:01 +0500

    Redundancy eliminated

--- PA1/task4/methods/gcsc.py
+++
@@ -1,8 +0,0 @@
-from methods.vanilla import VanillaMethod
-
-class GCSCMethod(VanillaMethod):
-    """
-    GCSC is identical to Vanilla in terms of architecture and loss.
-    The only difference is the dataset augmentation (handled in dataloader).
-    """
-    pass
